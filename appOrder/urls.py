fcc1b2c07134f6477de0ca48fea7abd04d38a580
harun-u-r-rashid <hr100756@gmail.com>
2024-06-01 22:30:12 +0600

    ssl update

--- appOrder/urls.py
+++ appOrder/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
         path('checkout/', views.checkout, name='checkout'),
         path('order_complete/', views.orderComplete, name='order_complete'),
-        path('success/', views.successView, name='success'),
-        path('fail/', views.failView, name='fail'),
+        path('payment/success/', views.successView, name='success'),
+        path('payment/fail/', views.failView, name='fail'),
         path('order_history/', views.orderHistory, name='history'),
         path('order_details/<int:orderId>', views.orderDetails, name='orderDetails'),
         path('payment/', views.payment, name='payment'),
