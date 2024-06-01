a0a2c62fcd29f558c2ed9ec7a679c8e6a73e8959
harun-u-r-rashid <hr100756@gmail.com>
2024-06-01 22:14:50 +0600

    updated trusted origin

--- onlineStore/settings.py
+++ onlineStore/settings.py
@@ -25,10 +25,10 @@ SECRET_KEY = 'django-insecure-unva6gqn21xvgb761a$mjq1i(eli=u!%)esqyixm2h4wv^-)#h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = [
-    'http://127.0.0.1:8000/'
+    'http://127.0.0.1:8000/', 'https://mens-online-store.onrender.com'
 ]
 
 
