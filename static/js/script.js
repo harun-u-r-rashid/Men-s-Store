732084b642fe347dcf81a146e0167d11f59d6e1d
harun-u-r-rashid <hr100756@gmail.com>
2024-07-12 06:20:15 +0600

    nav responsive

---
+++ static/js/script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+        const menu = document.querySelector(".menuLinks");
+        const icon = document.querySelector(".resIcon");
+        menu.classList.toggle("open");
+        icon.classList.toggle("open");
+      }
